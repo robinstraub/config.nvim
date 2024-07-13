41a0f180742f5f140965c04c7a786bce18f911dc
Robin Straub <robin@straub.pro>
2024-07-13 17:51:01 +0200

    feat: add lsp

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,72 @@
+return { -- LSP Configuration & Plugins
+  'neovim/nvim-lspconfig',
+  dependencies = {
+    { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
+    'williamboman/mason-lspconfig.nvim',
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+  },
+  config = function()
+    -- Configure the current buffer when an LSP attaches to it.
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+      callback = function(event)
+        -- Define a map function that lets us more easily define mappings specific for
+        -- LSP related item.
+        local map = function(keys, func, desc)
+          vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+        end
+
+        -- Jump to the definition of the word under your cursor.
+        --  This is where a variable was first declared, or where a function is defined, etc.
+        --  To jump back, press <C-t>.
+
+        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+
+        -- Opens a popup that displays documentation about the word under your cursor
+        --  See `:help K` for why this keymap.
+        map('K', vim.lsp.buf.hover, 'Hover Documentation')
+
+        -- Execute a code action, usually your cursor needs to be on top of an error
+        -- or a suggestion from your LSP for this to activate.
+        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+      end,
+    })
+
+    -- LSP servers and clients are able to communicate to each other what features they support.
+    --  By default, Neovim doesn't support everything that is in the LSP specification.
+    --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
+    --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    -- TODO: uncomment this once cmp_nvim_lsp is set up
+    -- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+
+    -- Enable the following language servers
+    local servers = {
+      intelephense = {},
+      lua_ls = {},
+      tsserver = {},
+    }
+
+    -- Ensure the servers and tools listed above are installed
+    require('mason').setup()
+
+    -- Add other tools to be installed by Mason
+    local ensure_installed = vim.tbl_keys(servers or {})
+
+    -- Handle install and upgrade of third-party lsp tools
+    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+    -- Set up servers installed by Mason
+    require('mason-lspconfig').setup {
+      handlers = {
+        function(server_name)
+          local server = servers[server_name] or {}
+          -- Handle overriding only values explicitly passed by the server configuration above
+          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+          -- Set up server via lspconfig
+          require('lspconfig')[server_name].setup(server)
+        end,
+      },
+    }
+  end,
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,13 +2,26 @@ return {
   'nvim-telescope/telescope.nvim',
   event = 'VimEnter',
   branch = '0.1.x',
-  dependencies = { 
+  dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons',
+    'nvim-telescope/telescope-ui-select.nvim'
   },
   config = function()
-    local builtin = require('telescope.builtin')
+    require('telescope').setup {
+      extensions = {
+        ['ui-select'] = {
+          require('telescope.themes').get_dropdown()
+        }
+      }
+    }
+
+    -- Enable Telescope extensions if they are installed
+    pcall(require('telescope').load_extension, 'fzf')
+    pcall(require('telescope').load_extension, 'ui-select')
+
     -- See `:help telescope.builtin`
+    local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]ile' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]ind [B]uffer' })
